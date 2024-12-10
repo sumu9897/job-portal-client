f1cb751aff45d9035e690bda49c01fb4465c07c7
sumu9897 <sumon.bncd@gmail.com>
2024-12-10 22:13:53 +0600

    register page design by lottie files

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import registerLottieData from '../../assets/lottie/register.json'
+import Lottie from 'lottie-react'
+
+const Register = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <div className="text-center lg:text-left w-96">
+      <Lottie animationData={registerLottieData}></Lottie>
+    </div>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <h1 className="text-5xl ml-8 mt-4 font-bold">Register now!</h1>
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Register</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default Register
