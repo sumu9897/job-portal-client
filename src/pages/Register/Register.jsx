03bff12781170b44ec1165ce09be00b4e6c5ca9c
sumu9897 <sumon.bncd@gmail.com>
2024-12-11 10:25:37 +0600

    User Create in Firebase

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import registerLottieData from '../../assets/lottie/register.json'
 import Lottie from 'lottie-react'
+import AuthContext from '../../context/AuthContext/AuthContext'
 
 const Register = () => {
+
+    const {createUser} = useContext(AuthContext);
     const handleRegister = e =>{
         e.preventDefault();
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
+
+        // password validation
+        // show password validation error
+
+        createUser(email , password )
+        .then(result =>{
+            console.log(result.user)
+        })
+        .catch(error =>{
+            console.log(error.message)
+        })
     }
   return (
     <div className="hero bg-base-200 min-h-screen">
