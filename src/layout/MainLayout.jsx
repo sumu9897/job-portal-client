0ee5f14df431cb8e3123667e1878eecd58264bad
sumu9897 <sumon.bncd@gmail.com>
2024-12-13 19:20:23 +0600

    Footer and navbar design done

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../pages/shared/Navbar";
-
-
+import Footer from "../pages/shared/Footer";
 
 const MainLayout = () =>{
     return(
         <div className="max-w-7xl mx-auto">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     )
 }
--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import AuthContext from '../../context/AuthContext/AuthContext'
-
+import logo from '../../assets/job-logo.png'
 
 const Navbar = () => {
   const {user , signOutUser} = useContext(AuthContext)
@@ -18,7 +18,8 @@ const Navbar = () => {
 
 
     const links = <>
-    <li><a>Item 1</a></li>    
+    <li>
+      <NavLink to={'/'}>Home</NavLink></li>    
     <li><a>Item 3</a></li>
     </>
   return (
@@ -45,7 +46,10 @@ const Navbar = () => {
         {links}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <a className="btn btn-ghost text-xl">
+      <img className='w-10' src={logo} alt="" />
+      <h3 className="text-3xl">Job Portal</h3>
+    </a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
