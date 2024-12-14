168c8db74e7d324302626ec409df7d7121babc4d
sumu9897 <sumon.bncd@gmail.com>
2024-12-14 13:50:02 +0600

    Banner design Ongoing

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "./Banner";
 
 
 const Home = () => {
     return(
         <div>
-            <h2>This is Home Page </h2>
+            <Banner></Banner>
         </div>
     );
 };
